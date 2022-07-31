77c9730da7738f918e1c184e8689c435731eafcc
Aashish <aashish.joshi@sonetel.com>
2022-07-31 21:24:45 +0530

    fix minor errors

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const port = process.env.port || 3000;
 const app = express();
 
 app.listen(port);
-console.log(`started on port ${port}`);
+
 app.set('view engine', 'ejs');
 app.use(express.static('includes'));
 
+// Routes
 app.get('/', (req, res) => {
     res.render('index');
 });
@@ -16,6 +17,7 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
+// Catchall route for 404
 app.use((req, res) => {
     res.status(404).render('404');
 });
